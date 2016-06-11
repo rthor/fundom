7cbd70f4adfd52ab4f96cc166e6bf4bf25f164ca
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2016-06-11 16:17:49 +0000

    Create state mixin

--- index.js
+++ index.js
@@ -1,43 +1,6 @@
-function createText() {
-  const node = document.createTextNode('')
+require('babel-register')
 
-  function setText(newText = '') {
-    node.innerText = newText
-    return setText
-  }
-
-  setText.node = node
-
-  return setText
+module.exports = {
+  component: require('./lib/component'),
+  createState: require('./lib/state'),
 }
-
-function createElement(type, props = {}) {
-  const node = Object.keys(props).reduce((element_, key) => {
-    element_.setAttribute(key, props[key])
-    return element_
-  }, document.createElement(type))
-
-  function element(child) {
-    if (typeof child === 'string') {
-      element.text = createText()(child).node
-      node.innerText = element.text.innerText
-    } else {
-      node.appendChild(child.node)
-    }
-    element.children.push(child)
-    return element
-  }
-
-  element.children = []
-  element.node = node
-  element.props = props
-  element.type = type.toLowerCase()
-
-  return element
-}
-
-function component(...args) {
-  return args.length ? createElement(...args) : createText()
-}
-
-export default component
